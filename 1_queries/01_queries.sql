e9cabda359b7d6fe1804b1b4b8ea265dfd9719fd
olicarignan <olive.carignan@gmail.com>
2019-10-16 20:57:29 +0000

    add queries

---
+++ 1_queries/01_queries.sql
@@ -0,0 +1,31 @@
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
+
+SELECT avg(end_date - start_date) as average_duration
+FROM reservations;
+
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
+
+SELECT properties.city, count(reservations) as total_reservations
+FROM reservations
+JOIN properties ON property_id = properties.id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
+
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
